48dcd0bd42f9220c8db7750d163e6fc53570eb99
Yudhistira Mauris <mauris.rx@gmail.com>
2015-08-22 21:54:01 +0700

    Initial commit, ready for 1.0

---
+++ includes/integrations/class-base.php
@@ -0,0 +1,33 @@
+<?php
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit();
+
+/**
+* AFFWP_PAFFL_Integrations_Base class
+*
+* This base class is responsible for managing affwp paffl integrations
+*
+* @since 1.0
+*/
+class AFFWP_PAFFL_Integrations_Base {
+
+	/**
+	 * Class __construct function
+	 *
+	 * @since 1.0
+	 */
+	public function __construct() {
+
+		
+		$this->load();
+	}
+
+	/**
+	 * Get things started
+	 *
+	 * @since 1.0
+	 */
+	public function load() {
+		
+	}
+}
---
+++ templates/product-affiliate-links.php
@@ -0,0 +1,35 @@
+<div id="affwp-affiliate-dashboard-product-affiliate-links" class="affwp-tab-content" style="padding-top: 20px;">
+
+	<h4><?php _e( 'Product Affiliate Links', 'affwp-paffl' ); ?></h4>
+
+	<table class="affwp-table">
+		<thead>
+			<tr>
+				<th><?php _e( 'Products', 'affwp-paffl' ); ?></th>
+				<th><?php _e( 'Commission', 'affwp-paffl' ); ?></th>
+				<th><?php _e( 'Affiliate Links', 'affwp-paffl' ); ?></th>
+			</tr>
+		</thead>
+
+		<tbody>
+
+			<?php $products     = affwp_paffl()->integrations->get_products(); ?>
+			<?php $referral_var = affwp_paffl()->integrations->referral_var; ?>
+			<?php $affiliate_id = affwp_get_affiliate_id(); ?>
+			<?php $rates		= affwp_paffl()->integrations->edd->get_products_referral_rates( $affiliate_id ); ?>
+
+			<?php foreach ( $products as $product ) : ?>
+
+			<tr>
+				<td><?php echo $product->post_title; ?></td>
+				<td><?php echo $rates[ $product->ID ]; ?></td>
+				<td><?php echo add_query_arg( array( $referral_var => $affiliate_id ), get_permalink( $product->ID ) ); ?></td>
+			</tr>
+
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+
+	<?php do_action( 'affwp_affiliate_dashboard_after_product_affiliate_links', affwp_get_affiliate_id() ); ?>
+
+</div>
